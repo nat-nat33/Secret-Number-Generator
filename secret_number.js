6e01a2d7b74fab92908d8593ed8fe868a85eb65d
Juan Jose Gonzalez Macias <JuanJo@MacBook-Pro-de-Juan-Jose-Gonzalez-Macias.local>
2016-03-08 20:18:31 -1000

    Final Secret Number

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
+
 module.exports = function() {
-  
+	var number;
+	number = Math.floor((Math.random() * 1000000) +1);
+
+	function secretNumber(){
+		return number;
+	
+	}
+	
+	return secretNumber;
 };
